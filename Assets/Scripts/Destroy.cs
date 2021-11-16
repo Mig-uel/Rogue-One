dda5e1db5d1dd642437d9ff993be791eb1795169
Miguel <miguelcastillo1000@gmail.com>
2021-11-15 20:39:17 -0500

    testing despawn

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -7,24 +7,24 @@ public class Destroy : MonoBehaviour
   // private GameObject enemy;
   void Start()
   {
-    anim = GetComponent<Animator>();
     // enemy = GameObject.FindWithTag("Enemy");
+    anim = GetComponent<Animator>();
   }
-  // private void OnCollisionEnter2D(Collision2D other)
-  // {
-  //   Destroy(gameObject, 0.5f);
-  // }
   private void OnTriggerEnter2D(Collider2D collision)
   {
-
-    if (collision.gameObject.CompareTag("Bound"))
-    {
-      gameObject.SetActive(false);
-    }
+    // if (collision.gameObject.CompareTag("Bound"))
+    // {
+    //   gameObject.SetActive(false);
+    // }
 
     explosionSoundEffect.Play();
 
     Destroy(gameObject, 0.5f);
     anim.Play("Explosion");
   }
+
+  // private void OnCollisionEnter2D(Collision2D other)
+  // {
+  //   Destroy(gameObject, 0.5f);
+  // }
 }
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
   [SerializeField] float speed;
+  private float lifetime;
   void Update()
   {
     transform.Translate(Vector3.up * -1 * speed * Time.deltaTime);
+
+    lifetime += Time.deltaTime;
+    // if (lifetime > 4) gameObject.SetActive(false);
+    if (lifetime > 4) Destroy(gameObject);
+
+  }
+  private void ResetLifetime()
+  {
+    lifetime = 0;
   }
 }
